77c8a5c667a76290ce6823563be3db2d652bd91c
Vinisha <mvinishareddy18@gmail.com>
2026-06-27 18:06:19 +0530

    example change

--- smartwater.cpp
+++ smartwater.cpp
@@ -5,7 +5,7 @@ using rgx::Regex;
 
 int main() {
     // 1. Compile your regex pattern
-    Regex re("(a|b)*abb"); 
+    Regex re("W(A|B)*[0-9]+"); 
 
     // 2. Print the Graphviz dot syntax directly to the console
     std::cout << re.dot() << std::endl;
